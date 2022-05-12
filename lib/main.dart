cd9d72952f55bd8fac7938b779f8dd7927b37e62
Aman Sharma <aman703jk@gmail.com>
2022-05-13 01:01:54 +0530

    learned about conatainer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Fluutter App",
+  runApp(const MaterialApp(
+    title: "Flutter App",
     home: HomePage(),
   ));
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Awesome Bro"),
       ),
-      body:  Container(
-        child: const Center(child: Text("Hi Flutter"))
-      ),
+      body:  Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          
+          width: 100,
+          height: 100,
+          decoration:  BoxDecoration(
+            boxShadow: [BoxShadow(color: Colors.grey.shade400, blurRadius: 5,  offset: Offset(2.0,5.0))],
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.purple,
+            gradient:  const LinearGradient(colors: [Colors.blue, Colors.green])
+          ),
+          child: const Text("I am a Box", style: TextStyle(color: Colors.white, fontSize: 18))
+        )
+      )
     );
   }
 }
