c8f0090a617e3f5e62919be1a7c597b0258d4977
Aman Sharma <aman703jk@gmail.com>
2022-05-13 00:20:45 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Fluutter App",
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Awesome Bro"),
+      ),
+      body:  Container(
+        child: const Center(child: Text("Hi Flutter"))
+      ),
+    );
+  }
+}
