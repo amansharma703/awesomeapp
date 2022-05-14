a3e02f94e7cfec64ceccaf5c0c4a410bee62b947
Aman Sharma <aman703jk@gmail.com>
2022-05-15 00:41:33 +0530

    text controller and card

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,107 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     title: "Flutter App",
     home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _nameController = TextEditingController();
+  var myText = "Change Me";
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text("Awesome Bro"),
       ),
-      body:  Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.black,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-              padding: const EdgeInsets.all(8),
-              
-              width: 50,
-              height: 100,
-              color: Colors.red,
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              
-              width: 50,
-              height: 100,
-              color: Colors.yellow,
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              
-              width: 50,
-              height: 100,
-              color: Colors.green,
-            )
-            ],
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(children: [
+              Image.asset("assets/bg.PNG"),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                myText,
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _nameController,
+                  keyboardType: TextInputType.text,
+                  // obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Something error",
+                      labelText: "Name",
+                      border: OutlineInputBorder()),
+                ),
+              )
+            ]),
           ),
         ),
-      )
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            // DrawerHeader(
+            //   child: Text(
+            //     "Hi I'am Drawer",
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+            //   decoration: BoxDecoration(color: Colors.purple),
+            // ),
+            UserAccountsDrawerHeader(
+              accountName: Text("Aman Sharma"),
+              accountEmail: Text("aman@aman.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage("url"),
+              ),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              trailing: Icon(Icons.send),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {myText = _nameController.text, setState(() {})},
+        child: const Icon(Icons.refresh),
+      ),
     );
   }
 }
+
+
+
+// error lens add this extension
