832944b0163f75fd0ec2611897eddf0bdf362be1
Aman Sharma <aman703jk@gmail.com>
2022-05-15 15:40:13 +0530

    loginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:awesomeapp/pages/homePage.dart';
+import 'package:awesomeapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Flutter App",
-    home: const HomePage(),
+    home: const LoginPage(),
     theme: ThemeData(primarySwatch: Colors.purple),
+    routes: {
+      "/login": (context) => LoginPage(),
+      "/Home": (context) => HomePage()
+    },
   ));
 }
-
-
-
-// error lens add this extension
